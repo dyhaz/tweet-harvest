f9a72859edbbcf4f74645ce951652f6a9f6bf6c3
hadi1 <m.dyhaz@gmail.com>
2024-12-09 01:09:17 +0700

    feat: add view count

--- src/dev.ts
+++ src/dev.ts
@@ -1,6 +1,16 @@
 import { crawl } from "./crawl";
 import { ACCESS_TOKEN } from "./env";
 
+// crawl({
+//   ACCESS_TOKEN: ACCESS_TOKEN,
+//   SEARCH_KEYWORDS: `miftah lang:id`,
+//   TARGET_TWEET_COUNT: 100,
+//   OUTPUT_FILENAME: "miftah.csv",
+//   DELAY_EACH_TWEET_SECONDS: 0.1,
+//   DELAY_EVERY_100_TWEETS_SECONDS: 0,
+//   SEARCH_TAB: "TOP",
+// });
+
 crawl({
   ACCESS_TOKEN: ACCESS_TOKEN,
   SEARCH_KEYWORDS: `gibran lang:id`,
